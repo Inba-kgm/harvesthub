4970e39145647bb40a87d50742b1d291885dcad2
Inba-kgm <subramanimani2080@gmail.com>
2025-08-19 16:30:55 +0530

    second commit

--- login/urls.py
+++ login/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('login_page',login_page,name='login_page'),
-    path('',login_page,name='login_page'),
+    path('',over,name='over'),
+    path('login',login_page,name='login_page'),
     path('signup_page',signup_page,name='signup_page'),
 ]
